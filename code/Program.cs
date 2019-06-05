8246d24728df1d5eac889bf64e121db82abd9706
Atif Aziz <code@raboof.com>
2019-06-05 19:05:47 +0200

    Import MoreLinq.Experimental namespace

--- code/Program.cs
+++ code/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using MoreLinq.Experimental;
 using MoreEnumerable = MoreLinq.MoreEnumerable;
 using OrderByDirection = MoreLinq.OrderByDirection;
 using static MoreLinq.Extensions.AcquireExtension;
